3f7d6292af67663767c3b3be35a8d1881a2c5396
Carlos Velasco <cvelasco@futurshealth.com>
2021-05-03 11:15:54 +0200

    rename microservice to patients

--- microservice/src/main/resources/db/migration/V1.1__CreateTablePatients.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS PATIENTS(
-   uuid varchar(100) NOT NULL,
-   name varchar(10) NOT NULL,
-   last_name varchar(10) NOT NULL,
-   date_created DATE,
-   birth_date DATE,
-   PRIMARY KEY (uuid )
-);
