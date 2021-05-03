3e7447cd3d11e772ec2962c480b8047547871778
Carlos Velasco <cvelasco@futurshealth.com>
2021-05-03 13:49:21 +0200

    add patient history microservice

--- patienthistory/src/main/java/com/geekshubs/patienthistory/infrastructure/workers/PatientCreatedKafkaWorker.java
+++ patienthistory/src/main/java/com/geekshubs/patienthistory/infrastructure/workers/PatientCreatedKafkaWorker.java
@@ -1,8 +1,12 @@
 package com.geekshubs.patienthistory.infrastructure.workers;
 
+import com.geekshubs.patienthistory.app.services.PatientHistoryService;
+import com.geekshubs.patienthistory.domain.entities.PatientHistory;
+import com.geekshubs.patienthistory.domain.exceptions.PatientHistoryException;
 import com.geekshubs.patienthistory.domain.models.Patient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +15,24 @@ public class PatientCreatedKafkaWorker {
 
     Logger log = LoggerFactory.getLogger(PatientCreatedKafkaWorker.class);
 
+    private PatientHistoryService patientHistoryService;
+
+    @Autowired
+    public PatientCreatedKafkaWorker(PatientHistoryService patientHistoryService) {
+        this.patientHistoryService = patientHistoryService;
+    }
+
     @KafkaListener(topics = "patients")
-    public void consume(Patient message) {
-        log.info("Mensaje recibido ->" + message.toString());
+    public void consume(Patient patient) throws PatientHistoryException {
+        log.info("Mensaje recibido ->" + patient.toString());
+        PatientHistory patientHistory = new PatientHistory(
+                patient.getName(),
+                patient.getLastName(),
+                patient.getUuid(),
+                "House",
+                "Some specialty"
+        );
+
+        this.patientHistoryService.save(patientHistory);
     }
 }
