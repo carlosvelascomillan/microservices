18dbae8af2a4bb1b595d20dc99bd99d367dabccd
Carlos Velasco <cvelasco@futurshealth.com>
2021-05-03 10:02:15 +0200

    add kafka

--- microservice/src/main/java/com/geekshubs/microservice/app/services/PatientServiceImpl.java
+++ microservice/src/main/java/com/geekshubs/microservice/app/services/PatientServiceImpl.java
@@ -1,31 +1,47 @@
 package com.geekshubs.microservice.app.services;
 
-import com.geekshubs.microservice.app.services.factories.FactoryPatients;
 import com.geekshubs.microservice.domain.entities.Patient;
 import com.geekshubs.microservice.domain.exceptions.PatientException;
 import com.geekshubs.microservice.domain.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @Service
 public class PatientServiceImpl implements PatientService {
 
     @Autowired
     private PatientRepository patientRepository;
+    private KafkaTemplate<String, Patient> kafkaTemplate;
+    private static final String TOPIC = "patients";
 
     @Override
     public Patient savePatient(Patient patient) throws PatientException {
 
+        Patient result = null;
+
         try {
-            return patientRepository.save(patient);
+            result = patientRepository.save(patient);
         } catch (Exception e) {
             throw new PatientException(e.getMessage());
         }
+
+        kafkaTemplate.send(TOPIC, result);
+        return result;
     }
 
     @Override
     public Mono<Patient> findPatientByUUID(String uuid) {
-        return Mono.just(FactoryPatients.toStream().findFirst().get());
+        Mono<Patient> monoPatient = Mono.fromCallable(() -> new Patient(
+                "pepe",
+                "pepe",
+                LocalDate.now(),
+                new Date()
+        ));
+        return monoPatient;
     }
 }
