60613ef9e65e81f841cf8fa36f57a51953db2f0f
Carlos Velasco <cvelasco@futurshealth.com>
2021-05-04 10:02:41 +0200

    Add keycloak

--- infrastructure/bd/01-database.sql
+++ infrastructure/bd/01-database.sql
@@ -1,12 +1,4 @@
 # create database
 CREATE DATABASE IF NOT EXISTS patients;
 CREATE DATABASE IF NOT EXISTS patientHistory;
-
-CREATE TABLE IF NOT EXISTS PATIENTS(
-   uuid varchar(100) NOT NULL,
-   name varchar(10) NOT NULL,
-   last_name varchar(10) NOT NULL,
-   date_created DATE,
-   birth_date DATE,
-   PRIMARY KEY (uuid )
-);
+CREATE DATABASE IF NOT EXISTS keycloak;
