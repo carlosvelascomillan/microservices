6c446ebd928a32232690dcf2a04df052ca42062d
Carlos Velasco <cvelasco@futurshealth.com>
2021-05-04 17:58:32 +0200

    add create table scripts

---
+++ patients/src/main/resources/db/migration/V1.1__CreateTablePatients.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS PATIENTS(
+   uuid varchar(100) NOT NULL,
+   name varchar(10) NOT NULL,
+   last_name varchar(10) NOT NULL,
+   date_created DATE,
+   birth_date DATE,
+   PRIMARY KEY (uuid )
+);
